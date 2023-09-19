1e17a058a01e49492ad853ebf2df04b7407cbdab
alyconr <alyconr@hotmail.com>
2023-09-19 16:50:57 -0500

    :wrench: Json Server Config

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ const port = process.env.PORT || 8000;
 server.use(middlewares);
 server.use(router);
 
+// cors middleware
+
+server.use((req, res, next) => {
+    
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+})
+
+
+
 server.listen(port, () => {
     console.log('JSON Server is running');
 })
